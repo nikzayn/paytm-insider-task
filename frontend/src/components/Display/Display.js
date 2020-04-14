b985eff48cc6254d344e6cf14844a6775b1f4c5e
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-14 23:48:48 +0530

    Display Component --initialized

---
+++ frontend/src/components/Display/Display.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>Processed Cropped Version</h1>
+            </div>
+        );
+    }
+}
+
+export default Display;
