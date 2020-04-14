a8806dd9fd3b95bb4607f57276316715740ceb48
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-14 23:49:01 +0530

    Upload Component --update

--- frontend/src/components/Upload/Upload.js
+++ frontend/src/components/Upload/Upload.js
@@ -1,39 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import 'react-dropzone-uploader/dist/styles.css'
 import Dropzone from 'react-dropzone-uploader'
 
-const Upload = () => {
-    const getUploadParams = () => {
+import Popup from '../Popup/Popup';
+
+
+class Upload extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            toggle: false,
+        }
+    }
+
+    toggleChange = (val) => {
+        this.setState({
+            toggle: !this.state.toggle,
+            val,
+        })
+    }
+
+    getUploadParams = () => {
         return { url: 'http://localhost:8080/upload' }
     }
 
-    const handleChangeStatus = ({ meta, remove }, status) => {
+    handleChangeStatus = ({ meta, remove }, status) => {
+        // if (status !== 'headers_received') {
+        //     console.log("Dialog");
+        // }
         if (status === 'headers_received') {
             console.log(`${meta.name} uploaded!`)
             remove()
+
+
         } else if (status === 'aborted') {
             console.log(`${meta.name}, upload failed...`)
         }
     }
 
-    return (
-        <div>
-            <Dropzone
-                getUploadParams={getUploadParams}
-                onChangeStatus={handleChangeStatus}
-                maxFiles={1}
-                accept='image/*'
-                multiple={false}
-                canCancel={false}
-                inputContent="Drop A File"
-                styles={{
-                    dropzone: { width: 400, height: 200, marginTop: '2em', overflow: 'hidden' },
-                    dropzoneActive: { borderColor: 'green' },
-                }}
-            />
-        </div>
-    );
+
+    render() {
+        return (
+            <div>
+                <button onClick={() => { this.toggleChange() }}>Popup</button>
+                {this.state.toggle ? <Popup toggle={this.state.toggle} toggleChange={this.toggleChange} /> : null}
+                <Dropzone
+                    getUploadParams={this.getUploadParams}
+                    onChangeStatus={this.handleChangeStatus}
+                    maxFiles={1}
+                    accept='image/*'
+                    multiple={false}
+                    canCancel={false}
+                    inputContent="Drop an Image"
+                    styles={{
+                        dropzone: { width: 400, height: 200, marginTop: '2em', overflow: 'hidden' },
+                        dropzoneActive: { borderColor: 'green' },
+                    }}
+                />
+            </div>
+        );
+    }
 }
+
+
 
 export default Upload;
