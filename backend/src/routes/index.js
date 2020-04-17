9615fead932e3e09ffd91109c3f811108d7233d6
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-17 20:17:39 +0530

    Endpoint route --update

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,27 +1,37 @@
 const routes = require('express').Router();
 const multer = require('multer');
 
-//Image Storage
+const cloudinary = require('../services/cloudinary');
+const { cloudUpload, checkImage } = cloudinary;
+
+//Image Folder
 const uploadFolder = 'src/uploads';
 const upload = multer({ dest: `${uploadFolder}` });
 
-const checkImage = require('../services/checkImage');
-const cloudUpload = require('../services/cloudUpload');
-
+//Upload Post
 routes.post('/upload', upload.any('file'), (req, res) => {
 
     const data = JSON.parse(req.body.dimensions);
     const location = `${uploadFolder}/${req.files[0].filename}`;
 
+    //Check image size
     checkImage(location, (ok) => {
         if (!ok) {
+            res.status(400)
+            res.send("please provide 1024x1024 image")
             return;
         }
-        cloudUpload(location, data, (result) => {
-            console.log(result)
-        })
-    });
 
+        cloudUpload(location, data, (error, result) => {
+            if (error) {
+                console.error(error)
+                res.status(500)
+                res.send("we are facing a technical difficulty. please try after sometime")
+                return;
+            }
+            res.send(result);
+        })
+    })
 });
 
 module.exports = routes;
