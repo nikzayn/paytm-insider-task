e117d32040623869e66ca3a923326eb7eced11da
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-17 20:17:21 +0530

    Server code --update

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,16 @@
-//Modules
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
 
-//Express Setup
+const routes = require('./routes');
 const app = express();
 
-//Port
 const port = 8080;
 
-//Cross Origin 
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', routes)
+app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
